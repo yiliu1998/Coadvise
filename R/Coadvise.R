37368f8b3aeb442aadcefe1005586f50c3ed7dcd
Yi Liu <90741171+yiliu1998@users.noreply.github.com>
2024-10-01 21:37:17 -0400

    updated missing data methods and function output

--- R/Coadvise.R
+++ R/Coadvise.R
@@ -1,31 +1,44 @@
-#' R function `Coadvise`: covariate adjustment with variable selection in randomized clinical trials
-#' @param y vector of outcome
-#' @param A vector of treatment (binary, valued from 0 and 1)
-#' @param X matrix of covariates or vector of a univariate covariate
-#' @param var.sel.method variable selection method:
-#'                       `No` (use all covariates without selection), `Lasso` (default), `A.Lasso`,
-#'                       `Corr.k` (select k covariates with highest correlations with the outcome; k is between 1 and number of covariates),
-#'                       `Corr.xi` (select all covariates with correlation > xi with the outcome; xi is between 0 and 1),
-#'                       `Pre.test` (preliminary test on each single covariate)
-#' @param lasso.family glm model family for Lasso (`Lasso`) variable selection based on outcome type, chosen from `gaussian`, `binomial`, `poisson`, and `multinomial`; default is `gaussian`
-#' @param A.lasso.family glm model family for adaptive Lasso (`A.Lasso`) variable selection based on outcome type, chosen from `gaussian`, `binomial`, `poisson`, and `multinomial`; default is `gaussian`
-#' @param out1.model.aipw model for E(Y(1)|X) in AIPW estimator. We allow the use of the following models:
-#'                        for continuous outcome (but can also for other types): `linear` (the linear model);
-#'                        for binary outcome: `logit`, `probit`, `log`, `cloglog`, `identity`;
-#'                        for categorical outcome more than two categories: `poisson`, `multilogi` (multinomial logistic), `ordlogi` (ordinal logistic)
-#' @param out0.model.aipw model for E(Y(0)|X) in AIPW estimator. Same argument choices as `out1.model.aipw`
-#' @param k number of selected covariates, only if the `Corr.k` variable selection method is used; default is `1`
-#' @param xi threshold of marginal correlation, only if the `Corr.xi` variable selection method is used; default is `0.25`
-#' @param pre.alpha confidence (significance) level of preliminary test; default is `0.05` (for a 0.95 confidence interval)
-#' @param conf.level confidence (significance) level of confidence interval of the ATE; default is `0.05` (for a 0.95 confidence interval)
-#' @param MI.method missing data imputation method if there is missing values in any outcome or covariates;
+#' Covariate adjustment with variable selection in randomized clinical trials
+#' @param Y Vector of observed outcomes
+#' @param A Vector of treatment assignments, which can be binary or multi-level, but values must be categorical or integers; if binary, we suggest to use 0 for the control and 1 for the treated group
+#' @param trt.name Value of the active treatment in the treatment vector `A`; must exactly match the value type in `A`, e.g., 1 for number while "1" for character
+#' @param ctrl.name Value of the control in the treatment vector `A`; must exactly match the value type in `A`, e.g., 0 for number while "0" for character
+#' @param X Covariate vector (only one covariate) or matrix/data frame (multiple covariates)
+#' @param var.sel.method Variable selection method for COADVISE stage I, including the following available options:
+#'                       `No` (Use all covariates without selection), `Lasso` (Lasso, which is the default choice), `A.Lasso` (Adaptive Lasso),
+#'                       `Corr.k` (Select k covariates with highest correlations with the outcome; k is between 1 and total number of covariates),
+#'                       `Corr.xi` (Select all covariates with correlation > xi with the outcome; xi is between 0 and 1),
+#'                       `Pre.test` (A preliminary two sample t-test on each single covariate)
+#' @param lasso.family GLM model family for Lasso (`Lasso`) variable selection based on outcome type, chosen from `gaussian`, `binomial`, `poisson`, and `multinomial`; default is `gaussian`
+#' @param A.lasso.family GLM model family for adaptive Lasso (`A.Lasso`) variable selection based on outcome type, chosen from `gaussian`, `binomial`, `poisson`, and `multinomial`; default is `gaussian`
+#' @param out1.model.aipw Model for E(Y(1)|X) in AIPW estimator. We allow the use of the following models:
+#'                        (i) For continuous outcome (but can also for other types): `linear` (the linear model);
+#'                        (ii) For binary outcome: `logit`, `probit`, `log`, `cloglog`, `identity`;
+#'                        (iii) For categorical outcome more than two categories: `poisson`, `multilogi` (multinomial logistic), `ordlogi` (ordinal logistic)
+#' @param out0.model.aipw Model for E(Y(0)|X) in AIPW estimator. Same argument choices as `out1.model.aipw`
+#' @param k Number of selected covariates, only if the `Corr.k` variable selection method is used; default is `1`
+#' @param xi Threshold of marginal correlation, only if the `Corr.xi` variable selection method is used; default is `0.25`
+#' @param pre.alpha Confidence (significance) level of preliminary test; default is `0.05` (for a 0.95 confidence interval)
+#' @param conf.level Confidence (significance) level of confidence interval of the ATE; default is `0.05` (for a 0.95 confidence interval)
+#' @param MI.method Missing data imputation method if there is missing values in any outcome or covariates;
 #'                  notice that the treatment is not allowed (or assumed) to be missing in RCT;
-#'                  values chosen from: `cc` (complete-case), `mice` (multiple imputation by chain equation), `miss-ind` (missingness indicator method)
-#' @param seed seed for generating random numbers, when using Lasso or adaptive Lasso; default is `4399`
-#' @return `df.fit` is a data frame containing results by Simple, ANCOVA, ANHECOVA and AIPW methods, including their point estimates, standard errors, CIs and p-values
-Coadvise <- function(y,
+#'                  values chosen from: `cc` (complete-case),
+#'                  `mice` (multiple imputation by chain equation),
+#'                  `missForest` (random forest),
+#'                  `missInd` (missingness indicator method) for missing covariates only, and
+#'                  `ipw` (inverse probability weighting) for missing outcome only.
+#'                  If there is no missing data, no method will be implemented even the value is specified.
+#' @param seed Seed for generating random numbers, when using Lasso or adaptive Lasso; default is `4399`
+#' @return The function returns a list of three components: (i) `df.fit`: results on ATE estimates, standard errors, CIs and p-values by
+#'         simple, ANCOVA, ANHECOVA and AIPW estimators under the chosen variable selection method; (ii) `stage1.covars`: a list of covariates
+#'         data frames by the first variable selection step, including the variable selection for all ANCOVA, ANHECOVA and AIPW methods;
+#'         (iii) `AIPW.out.means`: the vector of outcome means of the two comparison groups and the covariance matrix.
+Coadvise <- function(Y,
                      A,
+                     trt.name,
+                     ctrl.name,
                      X=NA,
+                     conti.out,
                      var.sel.method="Lasso",
                      lasso.family="gaussian",
                      A.lasso.family="gaussian",
@@ -38,6 +51,8 @@ Coadvise <- function(y,
                      MI.method="cc",
                      seed=4399) {
 
+  set.seed(seed=seed)
+
   if(!require("dplyr")) install.packages("dplyr")
   if(!require("glmnet")) install.packages("glmnet")
   if(!require("MASS")) install.packages("MASS")
@@ -45,23 +60,39 @@ Coadvise <- function(y,
   library(dplyr)
   library(glmnet)
 
+  if(!conti.out%in%c(TRUE, FALSE)) {
+    stop("Please specify whether the outcome variable is continuous:
+         if yes--conti.out=TRUE; if no--conti.out=FALSE")
+  }
+
+  if(!conti.out) {
+    Y <- as.factor(Y)
+  }
+
   if(sum(is.na(A))!=0) {
     stop("Missing values in the treatment vector is not supported!")
   }
 
-  if(sum(is.na(X))!=0 | sum(is.na(y))!=0) {
-    if(!MI.method%in%c("cc", "mice", "miss-ind")) {
+  group.ind <- which(A%in%c(trt.name, ctrl.name))
+  A <- as.character(A[group.ind])
+  A[A==as.character(trt.name)] <- "1"
+  A[A==as.character(ctrl.name)] <- "0"
+  A <- as.numeric(A)
+  Y <- Y[group.ind]
+  X <- X[group.ind,]
+
+  if(sum(is.na(X))!=0 | sum(is.na(Y))!=0) {
+    if(!MI.method%in%c("cc", "mice", "missInd", "missForest")) {
       stop("Missing data found in covariates or outcome!
-           Please select an imputation method from cc, mice, miss-ind,
+           Please select an imputation method from cc, mice, missInd, missForest,
            or please impute missing values before analysis.")
     }
 
     if(MI.method=="cc") {
       # complete-case analysis
-      dat.miss <- cbind(y, A, X)
+      dat.miss <- cbind(Y, A, X)
       dat.impu <- na.omit(dat.miss)
-      y <- dat.impu[,1]
-      A <- dat.impu[,2]
+      Y <- dat.impu[,1]
       X <- dat.impu[,-c(1,2)]
     }
 
@@ -69,19 +100,39 @@ Coadvise <- function(y,
       # multiple imputation by chained equation
       if(!require("mice")) install.packages("mice")
       library(mice)
-      dat.miss <- data.frame(y, A, X)
+      dat.miss <- data.frame(Y, A, X)
       dat.impu <- mice(dat.miss, m=5, method="pmm", maxit=50, seed=500)
       dat.impu <- complete(dat.impu, action=1)
-      y <- dat.impu[,1]
-      A <- dat.impu[,2]
+      Y.impu <- dat.impu[,1]
       X <- dat.impu[,-c(1,2)]
     }
 
-    if(MI.method=="miss-ind") {
-      if(sum(is.na(y))!=0) {
-        stop("Missing data found in outcome! The miss-ind method in current package
-             version only allows missing data in covariates. Please impute the outcome
-             by other methods first or use only complete-outcome data.")
+    if(MI.method=="missForest") {
+      # missing data imputation by random forest
+      if(!require("missForest")) install.packages("missForest")
+      library(missForest)
+      dat.miss <- data.frame(Y, A, X)
+      dat.impu <- missForest(dat.miss)$ximp
+      Y.impu <- dat.impu[,1]
+      X <- dat.impu[,-c(1,2)]
+    }
+
+    if(MI.method=="ipw") {
+      if(sum(is.na(X))!=0) {
+        stop("Missing data found in covariates! The IPW method cannot handle missing covariates.
+              Please consider other methods or use only complete-covariate data.")
+      }
+      dat.miss <- data.frame(Y, A, X)
+      obs_md <- glm(!is.na(Y)~A+X, family=binomial(), data=dat.miss)
+      p_obs <- predict(obs_md, type="response")
+      ipw <- ifelse(!is.na(Y), 1/p_obs, NA)
+      Y[is.na(Y)] <- weighted.mean(Y, ipw, na.rm=T)
+    }
+
+    if(MI.method=="missInd") {
+      if(sum(is.na(Y))!=0) {
+        stop("Missing data found in outcome! The missInd method cannot handle missing outcomes.
+              Please impute the outcome by other methods first or use only complete-outcome data.")
       }
       n.covar <- ncol(X)
       W <- c()
@@ -93,6 +144,8 @@ Coadvise <- function(y,
       }
       X <- cbind(W, X)
     }
+  } else {
+    Y.impu <- Y
   }
 
   ### centering covariates to their sample mean
@@ -103,18 +156,21 @@ Coadvise <- function(y,
   X0 <- X[A==0,]
   X.names <- colnames(X1) <- colnames(X0) <- colnames(X) <- paste("X.", 1:ncol(X), sep="")
   colnames(AX) <- paste("AX.", 1:ncol(AX), sep="")
-  Y <- y
+  Y <- as.numeric(Y.impu)
+  if(sum(levels(Y.impu)==c("0","1"))==2) {
+    Y[Y.impu=="0"] <- 0
+    Y[Y.impu=="1"] <- 1
+  }
   Y1 <- Y[A==1]
   Y0 <- Y[A==0]
   n <- length(A)
 
-  ### critical value for the confidence interval (quantile of standard normal)
+  ### Critical value for the confidence interval (quantile of standard normal)
   quant <- qnorm(1-conf.level/2)
-  set.seed(seed=seed)
 
   ### Simple estimator (unadjusted)
-  tau.simple <- mean(y[A==1])-mean(y[A==0])
-  se.simple <- sqrt(var(y[A==1])/sum(A)+var(y[A==0])/sum(1-A))
+  tau.simple <- mean(Y[A==1])-mean(Y[A==0])
+  se.simple <- sqrt(var(Y[A==1])/sum(A)+var(Y[A==0])/sum(1-A))
   ci.lwr.simple <- tau.simple - quant*se.simple
   ci.upr.simple <- tau.simple + quant*se.simple
   p.simple <- 2*(1-pnorm(abs(tau.simple/se.simple)))
@@ -124,9 +180,9 @@ Coadvise <- function(y,
     ind <- ind1 <- ind0 <- 1:ncol(X)
   }
   if(var.sel.method=="Lasso") {
-    cv.lasso <- cv.glmnet(X, y, alpha=1, family=lasso.family)
+    cv.lasso <- cv.glmnet(x=X, y=Y, alpha=1, family=lasso.family)
     lambda <- cv.lasso$lambda.min
-    lasso <- glmnet(X, y, alpha=1, lambda=lambda, family=lasso.family)
+    lasso <- glmnet(x=X, y=Y, alpha=1, lambda=lambda, family=lasso.family)
     ind <- which(as.numeric(lasso$beta)!=0)
 
     cv.lasso1 <- cv.glmnet(x=X1, y=Y1, alpha=1, family=lasso.family)
@@ -140,10 +196,10 @@ Coadvise <- function(y,
     ind0 <- which(as.numeric(lasso0$beta)!=0)
   }
   if(var.sel.method=="A.Lasso") {
-    cv.ridge <- cv.glmnet(X, y, alpha=0, family=A.lasso.family)
+    cv.ridge <- cv.glmnet(x=X, y=Y, alpha=0, family=A.lasso.family)
     w3 <- 1/abs(matrix(stats::coef(cv.ridge, s=cv.ridge$lambda.min)[,1][2:(ncol(X)+1)]))^1
     w3[w3[,1]==Inf] <- 999999999
-    alasso_cv <- cv.glmnet(X, y, type.measure="mse", nfold=5, alpha=1, penalty.factor=w3, keep=TRUE, family=A.lasso.family)
+    alasso_cv <- cv.glmnet(x=X, y=Y, type.measure="mse", nfold=5, alpha=1, penalty.factor=w3, keep=TRUE, family=A.lasso.family)
     alasso <- stats::coef(alasso_cv, s=alasso_cv$lambda.min, family=A.lasso.family)
     ind <- which(as.numeric(alasso)!=0)[-1]-1
 
@@ -164,7 +220,7 @@ Coadvise <- function(y,
   if(var.sel.method=="Corr.k") {
     if(k<1) stop("k should must be a positive integer!")
     if(k>ncol(X)) stop("k is larger than the number of all covariates!")
-    corr <- as.numeric(abs(cor(X, y)))
+    corr <- as.numeric(abs(cor(X, Y)))
     ind <- which(rank(corr)>=ncol(X)-(k-1))
 
     corr1 <- as.numeric(abs(cor(X1, Y1)))
@@ -243,8 +299,8 @@ Coadvise <- function(y,
 
   result <- .AIPW(Y1=Y1, Y0=Y0, X1=X1.AIPW, X0=X0.AIPW, A=A, X.pred=X, Y=Y,
                   out0.model=out0.model.aipw, out1.model=out1.model.aipw)
-  tau.aipw <- result$tau
-  se.aipw <- result$se
+  tau.aipw <- result$ATE$tau
+  se.aipw <- result$ATE$se
   p.aipw <- 2*(1-pnorm(abs(tau.aipw/se.aipw)))
   ci.lwr.aipw <- tau.aipw - quant*se.aipw
   ci.upr.aipw <- tau.aipw + quant*se.aipw
@@ -255,7 +311,17 @@ Coadvise <- function(y,
                        ci.lwr=c(ci.lwr.simple, ci.lwr.ancova, ci.lwr.anhecova, ci.lwr.aipw),
                        ci.upr=c(ci.upr.simple, ci.upr.ancova, ci.upr.anhecova, ci.upr.aipw),
                        p=c(p.simple, p.ancova, p.anhecova, p.aipw))
-  return(df.fit=df.fit)
+
+  stage1.covars <- list(ANCOVA=x.ancova[,-c(1,2)],
+                        ANHECOVA=x.anhecova[,-c(1,2)],
+                        AIPW.ctrl=X0.AIPW, AIPW.trt=X1.AIPW)
+
+  AIPW.out.means <- list(mu=result$mu, sigma=result$sigma)
+
+  return(list(df.fit=df.fit,
+              stage1.covars=stage1.covars,
+              AIPW.out.means=AIPW.out.means))
+
 }
 
 .AIPW <- function(Y0, Y1, X1, X0, A, X.pred, Y,
@@ -339,5 +405,7 @@ Coadvise <- function(y,
   v10 <- cov(Y1, m0.h1) + cov(Y0, m1.h0) - cov(m1.h, m0.h)
   se <- sqrt(v11-2*v10+v00)/sqrt(n)
 
-  return(fit=data.frame(tau=tau, se=se))
+  return(list(ATE=data.frame(tau=tau, se=se),
+              mu=data.frame(tau0=tau0, tau1=tau1),
+              sigma=matrix(c(v00,v10,v10,v11), ncol=2)))
 }
