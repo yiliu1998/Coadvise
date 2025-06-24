150075c5b4a65f557a77449c4daa4192db79e575
Yi Liu <90741171+yiliu1998@users.noreply.github.com>
2025-06-23 23:26:36 -0500

    ver 2.0: covariate-adaptive randomization added

--- R/CoadviseCAR.R
+++ R/CoadviseCAR.R
@@ -1,9 +1,10 @@
-#' Covariate adjustment with variable selection in randomized clinical trials
+#' Covariate adjustment with variable selection under covariate-adaptive randomization
 #' @param Y Vector of observed outcomes
 #' @param A Vector of treatment assignments, which can be binary or multi-level, but values must be categorical or integers; if binary, we suggest to use 0 for the control and 1 for the treated group
 #' @param trt.name Value of the active treatment in the treatment vector `A`; must exactly match the value type in `A`, e.g., 1 for number while "1" for character
 #' @param ctrl.name Value of the control in the treatment vector `A`; must exactly match the value type in `A`, e.g., 0 for number while "0" for character
 #' @param X Covariate vector (only one covariate) or matrix/data frame (multiple covariates)
+#' @param strata A discrete or factor type variable in the covariate-adaptive randomization (levels of stratification)
 #' @param conti.out Specify whether the outcome is continous, `TRUE` or `FALSE`, with no default value
 #' @param var.sel.method Variable selection method for COADVISE stage I, including the following available options:
 #'                       `No` (Use all covariates without selection), `Lasso` (Lasso, which is the default choice), `A.Lasso` (Adaptive Lasso),
@@ -31,35 +32,38 @@
 #'                  If there is no missing data, no method will be implemented even the value is specified.
 #' @param seed Seed for generating random numbers, when using Lasso or adaptive Lasso; default is `4399`
 #' @return The function returns a list of three components: (i) `df.fit`: results on ATE estimates, standard errors, CIs and p-values by
-#'         simple, ANCOVA, ANHECOVA and AIPW estimators under the chosen variable selection method; (ii) `stage1.covars`: a list of covariates
+#'         simple and AIPW estimators under the chosen variable selection method; (ii) `stage1.covars`: a list of covariates
 #'         data frames by the first variable selection step, including the variable selection for all ANCOVA, ANHECOVA and AIPW methods;
 #'         (iii) `AIPW.out.means`: the vector of outcome means of the two comparison groups and the covariance matrix.
-Coadvise <- function(Y,
-                     A,
-                     trt.name,
-                     ctrl.name,
-                     X=NA,
-                     conti.out,
-                     var.sel.method="Lasso",
-                     lasso.family="gaussian",
-                     A.lasso.family="gaussian",
-                     out1.model.aipw="linear",
-                     out0.model.aipw="linear",
-                     k=1,
-                     xi=0.25,
-                     pre.alpha=0.05,
-                     conf.level=0.05,
-                     MI.method="cc",
-                     seed=4399) {
+CoadviseCAR <- function(Y,
+                        A,
+                        trt.name,
+                        ctrl.name,
+                        strata,
+                        X=NA,
+                        conti.out,
+                        var.sel.method="Lasso",
+                        lasso.family="gaussian",
+                        A.lasso.family="gaussian",
+                        out1.model.aipw="linear",
+                        out0.model.aipw="linear",
+                        k=1,
+                        xi=0.25,
+                        pre.alpha=0.05,
+                        conf.level=0.05,
+                        MI.method="cc",
+                        seed=4399) {
 
   set.seed(seed=seed)
 
   if(!require("dplyr")) install.packages("dplyr")
+  if(!require("survey")) install.packages("survey")
   if(!require("glmnet")) install.packages("glmnet")
   if(!require("MASS")) install.packages("MASS")
   if(!require("nnet")) install.packages("nnet")
   library(dplyr)
   library(glmnet)
+  library(survey)
 
   if(!conti.out%in%c(TRUE, FALSE)) {
     stop("Please specify whether the outcome variable is continuous:
@@ -169,23 +173,37 @@ Coadvise <- function(Y,
   ### Critical value for the confidence interval (quantile of standard normal)
   quant <- qnorm(1-conf.level/2)
 
-  ### Simple estimator (unadjusted)
+  ### Simple estimator (no covariate, no strata)
   tau.simple <- mean(Y[A==1])-mean(Y[A==0])
   se.simple <- sqrt(var(Y[A==1])/sum(A)+var(Y[A==0])/sum(1-A))
   ci.lwr.simple <- tau.simple - quant*se.simple
   ci.upr.simple <- tau.simple + quant*se.simple
   p.simple <- 2*(1-pnorm(abs(tau.simple/se.simple)))
 
+  ### Simple estimator by stratification (no covariate)
+  df_strata <- data.frame(A=A, Y=Y, strata=strata)
+  summary_strata <- df_strata %>% group_by(strata) %>%
+    summarise(
+      n_s=n(), n1=sum(A==1), n0=sum(A==0),
+      y1_bar=mean(Y[A==1]), y0_bar=mean(Y[A==0]),
+      s1_sq=var(Y[A==1]), s0_sq=var(Y[A==0]),
+      tau_hat_s=y1_bar-y0_bar,
+      var_hat_tau_s=s1_sq / n1 + s0_sq / n0,
+      w_s=n_s/n
+    ) %>%
+    mutate(w_tau=w_s*tau_hat_s,
+           w_var=w_s^2*var_hat_tau_s)
+  tau.strat <- sum(summary_strata$w_tau)
+  se.strat <- sqrt(sum(summary_strata$w_var))
+  ci.lwr.strat <- tau.strat - quant*se.strat
+  ci.upr.strat <- tau.strat + quant*se.strat
+  p.strat <- 2*(1-pnorm(abs(tau.strat/se.strat)))
+
   ### Estimators by different variable selections
   if(var.sel.method=="No") {
     ind <- ind1 <- ind0 <- 1:ncol(X)
   }
   if(var.sel.method=="Lasso") {
-    cv.lasso <- cv.glmnet(x=X, y=Y, alpha=1, family=lasso.family)
-    lambda <- cv.lasso$lambda.min
-    lasso <- glmnet(x=X, y=Y, alpha=1, lambda=lambda, family=lasso.family)
-    ind <- which(as.numeric(lasso$beta)!=0)
-
     cv.lasso1 <- cv.glmnet(x=X1, y=Y1, alpha=1, family=lasso.family)
     lambda <- cv.lasso1$lambda.min
     lasso1 <- glmnet(x=X1, y=Y1, alpha=1, lambda=lambda, family=lasso.family)
@@ -197,32 +215,23 @@ Coadvise <- function(Y,
     ind0 <- which(as.numeric(lasso0$beta)!=0)
   }
   if(var.sel.method=="A.Lasso") {
-    cv.ridge <- cv.glmnet(x=X, y=Y, alpha=0, family=A.lasso.family)
-    w3 <- 1/abs(matrix(stats::coef(cv.ridge, s=cv.ridge$lambda.min)[,1][2:(ncol(X)+1)]))^1
-    w3[w3[,1]==Inf] <- 999999999
-    alasso_cv <- cv.glmnet(x=X, y=Y, type.measure="mse", nfold=5, alpha=1, penalty.factor=w3, keep=TRUE, family=A.lasso.family)
-    alasso <- stats::coef(alasso_cv, s=alasso_cv$lambda.min, family=A.lasso.family)
-    ind <- which(as.numeric(alasso)!=0)[-1]-1
-
     cv.ridge <- cv.glmnet(x=X1, y=Y1, alpha=0, family=A.lasso.family)
     w3 <- 1/abs(matrix(stats::coef(cv.ridge, s=cv.ridge$lambda.min)[,1][2:(ncol(X1)+1)]))^1
     w3[w3[,1]==Inf] <- 999999999
     alasso_cv1 <- cv.glmnet(x=X1, y=Y1, type.measure="mse", nfold=5, alpha=1, penalty.factor=w3, keep=TRUE, family=A.lasso.family)
-    alasso1 <- stats::coef(alasso_cv1, s = alasso_cv1$lambda.min, family=A.lasso.family)
+    alasso1 <- stats::coef(alasso_cv1, s=alasso_cv1$lambda.min, family=A.lasso.family)
     ind1 <- which(as.numeric(alasso1)!=0)[-1]-1
 
     cv.ridge <- cv.glmnet(x=X0, y=Y0, alpha=0, family=A.lasso.family)
     w3 <- 1/abs(matrix(stats::coef(cv.ridge, s=cv.ridge$lambda.min)[,1][2:(ncol(X0)+1)]))^1
     w3[w3[,1]==Inf] <- 999999999
     alasso_cv0 <- cv.glmnet(x=X0, y=Y0, type.measure="mse", nfold=5, alpha=1, penalty.factor=w3, keep=TRUE, family=A.lasso.family)
-    alasso0 <- stats::coef(alasso_cv0, s = alasso_cv0$lambda.min, family=A.lasso.family)
+    alasso0 <- stats::coef(alasso_cv0, s=alasso_cv0$lambda.min, family=A.lasso.family)
     ind0 <- which(as.numeric(alasso0)!=0)[-1]-1
   }
   if(var.sel.method=="Corr.k") {
     if(k<1) stop("k should must be a positive integer!")
     if(k>ncol(X)) stop("k is larger than the number of all covariates!")
-    corr <- as.numeric(abs(cor(X, Y)))
-    ind <- which(rank(corr)>=ncol(X)-(k-1))
 
     corr1 <- as.numeric(abs(cor(X1, Y1)))
     ind1 <- which(rank(corr1)>=ncol(X1)-(k-1))
@@ -232,8 +241,6 @@ Coadvise <- function(Y,
   }
   if(var.sel.method=="Corr.xi") {
     if(xi<=0 | xi>=1) stop("xi must be a number within (0,1)!")
-    corr <- as.numeric(abs(cor(X, Y)))
-    ind <- which(corr>xi)
 
     corr1 <- as.numeric(abs(cor(X1, Y1)))
     ind1 <- which(corr1>xi)
@@ -251,72 +258,33 @@ Coadvise <- function(Y,
       sd.x.diff <- sqrt(var(x1)/length(x1)+var(x0)/length(x0))
       test_x <- c(test_x, abs(x.diff/sd.x.diff) > qt(1-pre.alpha/2, df=length(Y)-2))
     }
-    ind <- ind1 <- ind0 <- ifelse(sum(test_x)>0, which(test_x), 0)
+    ind1 <- ind0 <- ifelse(sum(test_x)>0, which(test_x), 0)
   }
-  fit.ancova <- lm(Y~as.matrix(cbind(A, X[,ind])))
-  fit.anhecova <- lm(Y~as.matrix(cbind(A, X[,ind], AX[,ind])))
-  tau.ancova <- summary(fit.ancova)$coefficients[2,1]
-  tau.anhecova <- summary(fit.anhecova)$coefficients[2,1]
-  x.ancova <- cbind(1, A, X[,ind])
-  x.anhecova <- cbind(1, A, X[,ind], AX[,ind])
-  W <- as.matrix(X[,ind])
-
-  x <- x.ancova
-  sigma <- try(solve(t(x)%*%x)%*%t(x)%*%diag((fit.ancova$residuals)^2)%*%x%*%solve(t(x)%*%x))
-  if(class(sigma)[1]!="try-error") { sigma <- sigma } else { sigma <- NA }
-  if(sum(is.na(sigma))==0) { se.ancova <- sqrt(sigma[2,2]) } else { se.ancova <- NA }
-  p.ancova <- 2*(1-pnorm(abs(tau.ancova/(se.ancova))))
-  ci.lwr.ancova <- tau.ancova - quant*se.ancova
-  ci.upr.ancova <- tau.ancova + quant*se.ancova
-
-  x <- x.anhecova
-  sigma <- try(solve(t(x)%*%x)%*%t(x)%*%diag((fit.anhecova$residuals)^2)%*%x%*%solve(t(x)%*%x))
-  if(ncol(x)==2) {
-    if(class(sigma)[1]!="try-error") { se.anhecova <- sqrt(sigma[2,2]) } else { se.anhecova <- NA }
-  }
-  if(ncol(x)>2) {
-    W1 <- W[A==1,]
-    W0 <- W[A==0,]
-    fit1 <- summary(lm(Y1~as.matrix(W1)))$coefficients[-1,1]
-    fit0 <- summary(lm(Y0~as.matrix(W0)))$coefficients[-1,1]
-    sigma.add <- try(t(fit1-fit0)%*%cov(W)%*%(fit1-fit0) / n)
-    if(class(sigma)[1]!="try-error" & class(sigma.add)[1]!="try-error") {
-      sigma <- sigma
-      sigma.add <- sigma.add
-    } else {
-      sigma <- NA
-      sigma.add <- NA }
-    if(sum(is.na(c(sigma, sigma.add)))==0) {
-      se.anhecova <- sqrt(sigma[2,2]+sigma.add) } else { se.anhecova <- NA }
-  }
-  p.anhecova <- 2*(1-pnorm(abs(tau.anhecova/(se.anhecova))))
-  ci.lwr.anhecova <- tau.anhecova - quant*se.anhecova
-  ci.upr.anhecova <- tau.anhecova + quant*se.anhecova
 
   X1.AIPW <- as.matrix(X1[, ind1])
   colnames(X1.AIPW) <- X.names[ind1]
   X0.AIPW <- as.matrix(X0[, ind0])
   colnames(X0.AIPW) <- X.names[ind0]
 
-  result <- .AIPW(Y1=Y1, Y0=Y0, X1=X1.AIPW, X0=X0.AIPW, A=A, X.pred=X, Y=Y,
-                  out0.model=out0.model.aipw, out1.model=out1.model.aipw)
+  result <- .AIPW_CAR(Y1=Y1, Y0=Y0,
+                      X1=X1.AIPW, X0=X0.AIPW,
+                      A=A, X.pred=X, Y=Y,
+                      strata=strata,
+                      out0.model=out0.model.aipw, out1.model=out1.model.aipw)
   tau.aipw <- result$ATE$tau
   se.aipw <- result$ATE$se
   p.aipw <- 2*(1-pnorm(abs(tau.aipw/se.aipw)))
   ci.lwr.aipw <- tau.aipw - quant*se.aipw
   ci.upr.aipw <- tau.aipw + quant*se.aipw
 
-  df.fit <- data.frame(method=c("Simple", "ANCOVA", "ANHECOVA", "AIPW"),
-                       tau=c(tau.simple, tau.ancova, tau.anhecova, tau.aipw),
-                       se=c(se.simple, se.ancova, se.anhecova, se.aipw),
-                       ci.lwr=c(ci.lwr.simple, ci.lwr.ancova, ci.lwr.anhecova, ci.lwr.aipw),
-                       ci.upr=c(ci.upr.simple, ci.upr.ancova, ci.upr.anhecova, ci.upr.aipw),
-                       p=c(p.simple, p.ancova, p.anhecova, p.aipw))
+  df.fit <- data.frame(method=c("Simple", "Strata", "AIPW"),
+                       tau=c(tau.simple, tau.strat, tau.aipw),
+                       se=c(se.simple, se.strat, se.aipw),
+                       ci.lwr=c(ci.lwr.simple, ci.lwr.strat, ci.lwr.aipw),
+                       ci.upr=c(ci.upr.simple, ci.upr.strat, ci.upr.aipw),
+                       p=c(p.simple, p.strat, p.aipw))
 
-  stage1.covars <- list(ANCOVA=x.ancova[,-c(1,2)],
-                        ANHECOVA=x.anhecova[,-c(1,2)],
-                        AIPW.ctrl=X0.AIPW, AIPW.trt=X1.AIPW)
-
+  stage1.covars <- list(AIPW.ctrl=X0.AIPW, AIPW.trt=X1.AIPW)
   AIPW.out.means <- list(mu=result$mu, sigma=result$sigma)
 
   return(list(df.fit=df.fit,
@@ -325,8 +293,8 @@ Coadvise <- function(Y,
 
 }
 
-.AIPW <- function(Y0, Y1, X1, X0, A, X.pred, Y,
-                  out0.model="linear", out1.model="linear") {
+.AIPW_CAR <- function(Y0, Y1, X1, X0, A, X.pred, Y, strata,
+                      out0.model="linear", out1.model="linear") {
 
   n <- length(A)
   pi1 <- mean(A)
@@ -404,9 +372,26 @@ Coadvise <- function(Y,
   v11 <- var(Y1-m1.h1)/pi1 + 2*cov(Y1, m1.h1) - var(m1.h)
   v00 <- var(Y0-m0.h0)/pi0 + 2*cov(Y0, m0.h0) - var(m0.h)
   v10 <- cov(Y1, m0.h1) + cov(Y0, m1.h0) - cov(m1.h, m0.h)
-  se <- sqrt(v11-2*v10+v00)/sqrt(n)
+
+  Z <- factor(strata)
+  zlevels <- unique(Z)
+  OmSR <- matrix(c(pi0-pi0^2, -pi1*pi0, -pi1*pi0, pi1-pi1^2), 2, 2)
+  C <- matrix(0, 2, 2)
+  for (z in zlevels) {
+    idx0 <- Z[A==0]==z
+    idx1 <- Z[A==1]==z
+    idx <- Z==z
+    Qz <- diag(c((mean(Y0[idx0]-tau0) - mean(m0.h[idx]-mean(m0.h))) / pi0,
+                 (mean(Y1[idx1]-tau1) - mean(m1.h[idx]-mean(m1.h))) / pi1))
+    pi0z <- sum(A[idx]==0)/sum(Z==z)
+    pi1z <- sum(A[idx]==1)/sum(Z==z)
+    Omz <- matrix(c(pi0z-pi0z^2, -pi1z*pi0z, -pi1z*pi0z, pi1z-pi1z^2), 2, 2)
+    Cz <- Qz * (OmSR-Omz) * Qz * sum(Z==z)/n
+    C <- C+Cz
+  }
+  V <- matrix(c(v00,v10,v10,v11), 2, 2) - C
+  se <- sqrt(V[2,2]-2*V[1,2]+V[1,1])/sqrt(n)
 
   return(list(ATE=data.frame(tau=tau, se=se),
-              mu=data.frame(tau0=tau0, tau1=tau1),
-              sigma=matrix(c(v00,v10,v10,v11), ncol=2)))
+              mu=data.frame(tau0=tau0, tau1=tau1), sigma=V))
 }
